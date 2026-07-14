9d97adc8a405419c028511b1921837e68af0682b
Hafeez choudhary <choudharyhafeez2513@gmail.com>
2026-07-14 21:16:11 +0530

    Implement recursive request and folder counting

--- backend/analyzer/postman_analyzer.py
+++ backend/analyzer/postman_analyzer.py
@@ -17,3 +17,39 @@ def analyze_collection(json_data) :
     analysis["collection"]["postman_id"] = info.get("_postman_id")
 
     return analysis
+
+
+def analyze_summary(json_data):
+    summary = {
+        "summary": {
+            "total_requests": 0,
+            "total_folders": 0
+        }
+    }
+
+    items = json_data["item"]
+    request, folder = count_items(items)  
+    summary["summary"]["total_requests"] = request
+    summary["summary"]["total_folders"] = folder
+    
+
+    return summary
+
+
+def count_items(items):
+    request_count = 0
+    folder_count = 0
+ 
+    for item in items : 
+        if "request" in item :
+            request_count += 1 
+        
+        if "item" in item :
+            folder_count += 1 
+            sub_requests, sub_folders = count_items(item["item"])
+            request_count += sub_requests   
+            folder_count += sub_folders  
+
+    return request_count, folder_count
+
+
